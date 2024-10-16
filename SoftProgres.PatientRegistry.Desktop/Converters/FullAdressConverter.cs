1f47f76384337c5687e6ae9b852d2e7986dddd84
jayappsdev <jayappsdev@gmail.com>
2024-10-16 13:22:14 +0200

    first frontend commit

---
+++ SoftProgres.PatientRegistry.Desktop/Converters/FullAdressConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SoftProgres.PatientRegistry.Desktop.Converters;
+
+public class FullAdressConverter : IMultiValueConverter
+{
+    private static readonly char[] trimChars = [',', ' '];
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        string streetAndNumber = values[0] as string;
+        string postalCode = values[1] as string;
+        string city = values[2] as string;
+        string state = values[3] as string;
+
+        return $"{streetAndNumber}, {postalCode}, {city}, {state}".Trim(trimChars);
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
